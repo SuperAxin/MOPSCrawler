6ebda88b58ed91f384e12ba27d43e41d8f645236
SuperAxin <smartaxin@gmail.com>
2024-04-05 22:15:10 +0800

    FinancialStatement Catch

--- Spider.py
+++ Spider.py
@@ -1,16 +1,28 @@
 import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
-class GET:
-    def __init__(self, url):
+class FinancialStatementFetcher:
+    def __init__(self, url, types, coid, year, season): # types = [0:資產負債, 1:損益, 2:現金流量, 3:權益變動]
         self.url = url
+        self.params = {
+            'step': '1',
+            'CO_ID': str(coid),
+            'SYEAR': year,
+            'SSEASON': season,
+            'REPORT_ID': 'C'
+        }
+        self.types = types
+        self.content = None
 
     def send_request(self):
         try:
-            response = requests.get(self.url)
-            # 檢查是否成功收到回應
+            response = requests.post(self.url, params=self.params)
             if response.status_code == 200:
                 print("Request successful")
-                return response.text
+                content = response.content.decode('cp950')
+                self.content = content
+                return "OK"
             else:
                 print("Request failed with status code:", response.status_code)
                 return None
@@ -18,9 +30,48 @@ class GET:
             print("Request failed:", e)
             return None
 
-# 使用範例
-url = "https://mops.twse.com.tw/mops/web/index"
-getter = GET(url)
+    def catch_table(self):
+        if self.content is not None:  # 检查内容是否已经获取
+            soup = BeautifulSoup(self.content, 'html.parser')
+            div_content = soup.find('div', class_='content')
+            if div_content:
+                tables = div_content.find_all('table')[self.types:self.types+1]  # 只取第一個table
+                for table in tables:
+                    rows = table.find_all('tr')[2:]  # 從第三個tr開始
+                    Chinese_text = []
+                    Money = []
+                    for row in rows:
+                        datas = row.find_all('td')[1:2]  # 只抓會計科目
+                        for data in datas:
+                            text = data.find('span', class_='zh').text.strip()
+                            Chinese_text.append(text)
+                        datas = row.find_all('td')[2:3]  # 只抓金額
+                        for data in datas:
+                            number = data.text
+                            Money.append(number)
+
+                    print(' ')
+                    d = {
+                        'Accounts': Chinese_text,
+                        'Money': Money
+                    }
+
+                    df = pd.DataFrame(data=d)
+
+            return df
+        else:
+            print("No content available. Please call send_request first.")
+            return None
+
+
+'''url = 'https://mops.twse.com.tw/server-java/t164sb01'
+types = 3
+coid = 2330
+year = 2020
+season = 1
+
+getter = FinancialStatementFetcher(url, types, coid, year, season)
 response_text = getter.send_request()
-if response_text:
-    print(response_text)
+table = getter.catch_table()
+print(table)'''
+
--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 
 
 
-from Spider import GET
+from Spider import FinancialStatementFetcher
 
 def print_hi(name):
     print(f'Hi, {name}')
 
 if __name__ == '__main__':
-    url = "https://example.com"
-    getter = GET(url)
+    url = 'https://mops.twse.com.tw/server-java/t164sb01'
+    types = 3
+    coid = 2330
+    year = 2020
+    season = 1
+
+    getter = FinancialStatementFetcher(url, types, coid, year, season)
     response_text = getter.send_request()
-    if response_text:
-        print_hi(response_text)
+    table = getter.catch_table()
+    print(table)
 
